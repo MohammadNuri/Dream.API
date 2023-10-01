f208d41bd0e8a2a25e5a7e35ae6771f4dab12fd9
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2023-10-01 17:42:59 +0330

    +Setup Project PipeLine from Begin +First Program.cs Pipeline Configuration

--- Dream.API/Program.cs
+++ Dream.API/Program.cs
@@ -3,23 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+#region PipeLine
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+    app.Run(async (context) =>
+    {
+        await context.Response.WriteAsync("Wrong Request (Wrong URL Perhaps)");
+    });
 
-app.UseHttpsRedirection();
+#endregion
 
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
