bab69cb79f07dabaa30e50f606cc193d784b72d7
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2023-10-02 12:09:02 +0330

    +City DataTransferObject (Dto) +InMemory Data (Just for Practice) Later we Use SQL and EFcore (as a Singleton Algorithm) Domain/api/Cities

---
+++ Dream.API/CitiesDataStore.cs
@@ -0,0 +1,19 @@
+﻿using Dream.API.Models;
+
+namespace Dream.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+               new CityDto() { Id = 1, Name = "Tehran", Description = "This is for Tehran" },
+               new CityDto() { Id = 2, Name = "Shiraz", Description = "This is for Shiraz" },
+               new CityDto() { Id = 3, Name = "Ahwaz", Description = "This is for Ahwaz" }
+            };
+        }
+    }
+}
--- Dream.API/Controllers/CitiesController.cs
+++ Dream.API/Controllers/CitiesController.cs
@@ -3,18 +3,13 @@
 namespace Dream.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]    
+    [Route("api/Cities")]    
     public class CitiesController : ControllerBase
     {
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<object>
-                {
-                    new {id =  1, name = "Tehran"},
-                    new {id =  2, name = "Shiraz"}
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
